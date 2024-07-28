8f09dd876a82b07cd8800e5ac9d7757d35ba7287
bssdka <ay.zhiliaev@outlook.com>
2024-07-28 16:27:19 +0300

    commit

--- src/basic-todo.py
+++ src/basic-todo.py
@@ -17,7 +17,6 @@ __system__ = platform.system()
 config = cfg.ConfigParser()
 config.read(f'{__path__}/config/basic.ini')
 meta = config['DEFAULT']
-widget = config['widget.SIZES']
 theme = config['system.THEME']
 font = config['FONT']
 __theme__ = meta['theme'] # global variable
@@ -34,18 +33,13 @@ class MainWindow(ctk.CTk):
 
         self.initUI()
     def initUI(self):
-        # Definition of function variables 
-        # mw_btn - main_width_btn, mh_btn - main_height_btn, ss_btn - settings_size_btn, w_scrlframe - width_scrollframe,
-        # h_scrollframe - height_scrollframe
-        mw_btn, mh_btn, ss_btn = int(widget['width-main-btn']), int(widget['height-main-btn']), int(widget['size-settings-btn'])
-        w_scrframe, h_scrollframe = int(widget['width-scrollframe']), int(widget['height-scrollframe'])
 
         # -----CREATINGE AND POSITIONING WIDGETS-----
-        create_node_btn = ctk.CTkButton(self, width=mw_btn, height=mh_btn, text="CREATE", font=self.__font__,).grid(row=0, column=0, padx=25, pady=25)
-        edit_node_btn = ctk.CTkButton(self, width=mw_btn, height=mh_btn, text="EDIT", font=self.__font__,).grid(row=0, column=1, pady=25)
-        settings_btn = ctk.CTkButton(self, image=settings_image, width=ss_btn, height=ss_btn, text="", command=self.launch_settings).grid(row=0, column=2, padx=25, pady=25)
+        create_node_btn = ctk.CTkButton(self, width=100, height=50, text="CREATE", font=self.__font__,).grid(row=0, column=0, padx=25, pady=25)
+        edit_node_btn = ctk.CTkButton(self, width=100, height=50, text="EDIT", font=self.__font__,).grid(row=0, column=1, pady=25)
+        settings_btn = ctk.CTkButton(self, image=settings_image, width=50, height=50, text="", command=self.launch_settings).grid(row=0, column=2, padx=25, pady=25)
 
-        scrollframe = ctk.CTkScrollableFrame(self, width=w_scrframe, height=h_scrollframe, fg_color='#4c4e52', label_anchor='e',).place(x=25, y=100)
+        scrollframe = ctk.CTkScrollableFrame(self, width=275, height=450, fg_color='#4c4e52', label_anchor='e',).place(x=25, y=100)
 
         self.build_theme()
         self.launch_settings() # ---DEBUG---
@@ -54,18 +48,17 @@ class MainWindow(ctk.CTk):
     def build_theme(self):
         for widget in self.winfo_children():
             if widget.winfo_class() == 'Frame':
-                
-                try:
+                index = str(widget).find('button')
+                if index != -1:
                     if __theme__ == 'light': 
                         widget.configure(fg_color=f'#{theme['light-foreground-btn']}',
-                                        hover_color=f'#{theme['light-hover-btn']}', text_color='#000000')
+                                            hover_color=f'#{theme['light-hover-btn']}', text_color='#000000')
                         self.configure(fg_color=f'#{theme['light-foreground']}')
                     if __theme__ == 'dark':
                         widget.configure(fg_color=f'#{theme['dark-foreground-btn']}',
-                                        hover_color=f'#{theme['dark-hover-btn']}', text_color='#ffffff')
+                                            hover_color=f'#{theme['dark-hover-btn']}', text_color='#ffffff')
                         self.configure(fg_color=f'#{theme['dark-foreground']}')
-                except ValueError:
-                    pass
+                    
 
     def launch_settings(self):
         # if self.toplevel_window is None or not self.toplevel_window.winfo_exists():
@@ -87,6 +80,6 @@ if __name__ == '__main__':
 
 
 """ TODO
-    1) Решить проблему .configure у CTkScrollableFrame
+    1) Решить проблему .configure у CTkScrollableFrame not working
     2) Поменять алгоритм установки темы
 """
--- src/functions/settings.py
+++ src/functions/settings.py
@@ -2,6 +2,9 @@
 
 import customtkinter as ctk
 import configparser as cfg
+from elevate import elevate
+import subprocess
+import sys
 import os
 
 # -----GLOBAL VARIABLES-----
@@ -15,6 +18,14 @@ theme = config['system.THEME']
 font = config['FONT']
 __theme__ = meta['theme'] # global variable
 
+# Define values for rendering the correct theme for widgets 
+if __theme__ == 'light': 
+    __color__ = '#000000'
+    __btn_change_theme__ = 'DARK THEME'
+if __theme__ == 'dark': 
+    __color__ = '#ffffff'
+    __btn_change_theme__ = 'LIGHT THEME'
+
 
 class SettingsWindow(ctk.CTkToplevel):
     def __init__(self, *args, **kwargs):
@@ -22,34 +33,46 @@ class SettingsWindow(ctk.CTkToplevel):
         self.__font__ = ctk.CTkFont(family=font['family'], size=int(font['size'])+4, weight=font['weight'])
 
         self.geometry(f'{meta['width_settings']}x{meta['height_settings']}')
-        # I don't know why function self.configure(fg_color) not working, but it works here. wtf...
-        if __theme__ == 'light': 
-            self.configure(fg_color=f'#{theme['light-foreground']}') 
-            self.__color__ = '#000000'
-        if __theme__ == 'dark': 
-            self.configure(fg_color=f'#{theme['dark-foreground']}')
-            self.__color__ = '#ffffff'
 
         self.initUI()
     def initUI(self):
         # -----BUILD WIDGETS-----
-        label_switch = ctk.CTkLabel(self, text='SWITCH\nTHEME', width=150, height=50, font=self.__font__, text_color=self.__color__).grid(row=0, column=0, pady=25)
-        theme_switch = ctk.CTkSwitch(self, switch_width=75, switch_height=40, text="").grid(row=0, column=1, padx=0, pady=25)
+        label_switch = ctk.CTkLabel(self, text='SWITCH\nTHEME', width=100, height=50, font=self.__font__, text_color=__color__).grid(row=0, column=0, padx=25, pady=25)
+        light_theme_btn = ctk.CTkButton(self, text=__btn_change_theme__, width=200, height=50, font=self.__font__, text_color=__color__, command=self.change_theme).grid(row=0, column=1, padx=25, pady=25)
 
+        # Init theme
         self.build_theme()
 
 
     def build_theme(self):
         for widget in self.winfo_children():
-            index = str(widget).find('button')
-            if index != -1:
-                if __theme__ == 'light': 
-                    widget.configure(fg_color=f'#{theme['light-foreground-btn']}',
-                                hover_color=f'#{theme['light-hover-btn']}', text_color='#000000')
-                if __theme__ == 'dark':
-                    widget.configure(fg_color=f'#{theme['dark-foreground-btn']}',
-                                hover_color=f'#{theme['dark-hover-btn']}', text_color='#ffffff')
-            
+            if widget.winfo_class() == 'Frame':
+                index = str(widget).find('button')
+                if index != -1:
+                    if __theme__ == 'light': 
+                        widget.configure(fg_color=f'#{theme['light-foreground-btn']}',
+                                            hover_color=f'#{theme['light-hover-btn']}', text_color='#000000')
+                        self.configure(fg_color=f'#{theme['light-foreground']}')
+                    if __theme__ == 'dark':
+                        widget.configure(fg_color=f'#{theme['dark-foreground-btn']}',
+                                            hover_color=f'#{theme['dark-hover-btn']}', text_color='#ffffff')
+                        self.configure(fg_color=f'#{theme['dark-foreground']}')
+
+
+    def change_theme(self):
+        # Change configuration file
+        if __theme__ == 'light': config.set('DEFAULT', 'theme', 'dark')
+        if __theme__ == 'dark': config.set('DEFAULT', 'theme', 'light')
+        with open(f'{__path__}/config/basic.ini', 'w') as configfile:
+            config.write(configfile)
+        elevate(graphical=False)
+        # -RESTART APP-
+        subprocess.run([f'{__path__}/src/restart.sh'])
+        raise SystemExit(0)
+        
+        
+ 
+
 """ TODO
     1) Реализовать темы для виджетов
     2) Поменять switch на 2 кнопки
